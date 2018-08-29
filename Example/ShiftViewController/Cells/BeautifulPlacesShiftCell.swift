79fa2289799b6fa472c8f8a4f8cd544603aed71d
David Martinez <david.martinez@innocv.com>
2018-08-29 19:48:59 +0200

    Example - Include new features to the example

--- Example/ShiftViewController/Cells/BeautifulPlacesShiftCell.swift
+++ Example/ShiftViewController/Cells/BeautifulPlacesShiftCell.swift
@@ -10,13 +10,32 @@ import SDWebImage
 class BeautifulPlacesShiftCell: ShiftCardViewCell {
 
     @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var veilContainerView: UIView!
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var countryLabel: UILabel!
     @IBOutlet private weak var photoImageView: UIImageView!
     @IBOutlet private weak var loadingView: UIActivityIndicatorView!
 
+    @IBOutlet private weak var likeLabel: UILabel!
+    @IBOutlet private weak var dislikeLabel: UILabel!
+    @IBOutlet private weak var superlikeLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = veilContainerView.bounds
+        let gradientColors = [
+            UIColor.black.withAlphaComponent(0.0).cgColor,
+            UIColor.black.withAlphaComponent(0.1).cgColor,
+            UIColor.black.withAlphaComponent(0.1).cgColor,
+            UIColor.black.withAlphaComponent(0.2).cgColor,
+            UIColor.black.withAlphaComponent(0.3).cgColor,
+            UIColor.black.withAlphaComponent(0.5).cgColor,
+            UIColor.black.withAlphaComponent(0.8).cgColor,
+        ]
+        gradientLayer.colors = gradientColors
+        veilContainerView.layer.addSublayer(gradientLayer)
 
         let colors = [UIColor.red, .blue, .yellow, .orange, .brown, .cyan, .gray, .green]
         let color = colors[Int(arc4random()) % colors.count]
@@ -39,10 +58,30 @@ class BeautifulPlacesShiftCell: ShiftCardViewCell {
     // MARK: Overrides
 
     override func cellPanShift(with percent: CGFloat, andDirection direction: ShiftCardDirection?) {
+        guard let direction = direction else { return }
+        switch direction {
+        case .topLeft, .top, .topRight:
+            setAlphaPercent(percent: percent, to: superlikeLabel)
+        case .left, .bottomLeft:
+            setAlphaPercent(percent: percent, to: dislikeLabel)
+        case .right, .bottomRight:
+            setAlphaPercent(percent: percent, to: likeLabel)
+        default:
+            break
+        }
+    }
 
+    private func setAlphaPercent(percent: CGFloat, to label:UILabel) {
+        [likeLabel, dislikeLabel, superlikeLabel].filter({ $0 != label })
+            .forEach({ $0.alpha = 0 })
+        label.alpha = percent
     }
 
     override func cellReset(animationDuration: Double) {
-
+        UIView.animate(withDuration: animationDuration) { [unowned self] () -> Void in
+            self.likeLabel.alpha = 0.0
+            self.dislikeLabel.alpha = 0.0
+            self.superlikeLabel.alpha = 0.0
+        }
     }
 }
